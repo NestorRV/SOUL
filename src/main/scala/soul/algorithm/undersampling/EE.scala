06a3b9e28ad644557429d2d2af4636457718fba6
Néstor Rodríguez Vico <nestorrodriguezvico@gmail.com>
2018-10-02 21:19:54 +0200

    FIx doc.

--- src/main/scala/soul/algorithm/undersampling/EE.scala
+++ src/main/scala/soul/algorithm/undersampling/EE.scala
@@ -6,7 +6,7 @@ import soul.util.Utilities._
 
 import scala.util.Random
 
-/** Easy Ensemble core. Original paper: "Exploratory Undersampling for Class-Imbalance Learning" by Xu-Ying Liu, Jianxin Wu and Zhi-Hua Zhou.
+/** Easy Ensemble algorithm. Original paper: "Exploratory Undersampling for Class-Imbalance Learning" by Xu-Ying Liu, Jianxin Wu and Zhi-Hua Zhou.
   *
   * @param data          data to work with
   * @param seed          seed to use. If it is not provided, it will use the system time
